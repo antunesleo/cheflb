dbe8cca7c2c559a0ad1a4248974c25d6199fdd14
Leo Antunes <antunesleo4@gmail.com>
2025-01-24 01:00:59 -0300

    implement draft of redirect strategy

--- internal/server.go
+++ internal/server.go
@@ -1,20 +1,54 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
+	// "time"
 )
+
+const forwardMode = "request" // request or redirect
 
 type LbHandler struct {}
 
 func (mh *LbHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	fmt.Println("Request received!")
 	url := fmt.Sprintf("http://localhost:7171%s", r.URL.Path)
-	rw.Header().Add(
-		"Location", url,
-	)
-	rw.WriteHeader(307)
+	
+	if forwardMode == "redirect" {
+		rw.Header().Add(
+			"Location", url,
+		)
+		rw.WriteHeader(307)
+	} else {
+		client := &http.Client{}
+		var reqBuffer []byte
+		_, err := r.Body.Read(reqBuffer)
+		if err != nil {
+			return
+		}
+		
+	
+		req, err := http.NewRequest(r.Method, url, bytes.NewReader(reqBuffer))
+		if err != nil {
+			return
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			return
+		}
+
+		var respBuffer []byte
+		_, err = resp.Body.Read(respBuffer)
+		if err != nil {
+			return
+		}
+		rw.Write(respBuffer)
+		rw.WriteHeader(resp.StatusCode)
+
+	}
+
 }
 
 func Start() {
